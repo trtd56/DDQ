58724e0b8d8ef04cd0fb90c74a25ca6d737891ec
trtd56 <5toda6@gmail.com>
2019-02-08 14:23:48 +0900

    add: my script

---
+++ src/test.sh
@@ -0,0 +1,15 @@
+pipenv run python run.py --agt 9 \
+              --usr 1 \
+              --max_turn 40 \
+              --movie_kb_path ./deep_dialog/data/movie_kb.1k.p \
+              --dqn_hidden_size 80 \
+              --experience_replay_pool_size 1000 \
+              --episodes 300 \
+              --simulation_epoch_size 100 \
+              --write_model_dir ./deep_dialog/checkpoints/DDQAgent/ \
+              --slot_err_prob 0.00 \
+              --intent_err_prob 0.00 \
+              --batch_size 16 \
+              --goal_file_path ./deep_dialog/data/user_goals_first_turn_template.part.movie.v1.p \
+              --trained_model_path ./deep_dialog/checkpoints/DDQAgent/agt_9_225_500_0.58800.pkl \
+              --run_mode 3
